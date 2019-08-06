5c34814b94f5fcfe4791021fd34284f4f9c655d7
ajuarez <ajuarezcr97@gmail.com>
2019-08-06 19:56:38 -0400

    Initial commit.

---
+++ ProjectPlan/ProjectPlan/Models/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectPlan.Models
+{
+    public class Project
+    {
+        public int UserId { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int Credits { get; set; }
+    }
+}
---
+++ ProjectPlan/ProjectPlan/Scripts/main.js
@@ -0,0 +1,48 @@
+﻿ProjectPlan = function () {
+    this.routes.getUserInformation = "HomeController/GetUserInformation";
+
+    this.bindEvents();
+}
+
+ProjectPlan.prototype.bindEvents = function () {
+    var self = this;
+
+    $("#btn_findUser").click(self.getUserInformation);
+}
+
+ProjectPlan.prototype.getUserInformation = function () {
+    var self = this;
+
+    $.ajax({
+        url: self.routes.getUserInformation,
+        data: "{ userId : '" + id + "' }",
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+            if (result) {
+                self.fillUserTable(result);
+            }
+        }
+    });
+}
+
+ProjectPlan.prototype.fillUserTable = function (projects) {
+    var dataLength = projects.length;
+    var htmlContent = "<tbody>" + "<tr>";
+
+    for (var i = 0; i < dataLength; i++) {
+        var project = projects[i];
+
+        htmlContent += "<th scope='row'>" + project.Id + "</th>";
+        htmlContent += "<th>" + project.StartDate + "</th>";
+        htmlContent += "<th>" + project.TimeToStart + "</th>";
+        htmlContent += "<th>" + project.EndDate + "</th>";
+        htmlContent += "<th>" + project.Credits + "</th>";
+        htmlContent += "<th>" + project.Status + "</th>";
+    }
+
+    htmlContent += " </tr >" + "</tbody>";
+
+    $("#table_userInformation").append(htmlContent);
+}
